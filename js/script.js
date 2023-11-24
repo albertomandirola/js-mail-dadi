b98ec1f1eabdfbd7c175cc963bcebe5809bd4add
albertomandirola <albymandi00@gmail.com>
2023-11-24 10:05:51 +0100

    structure of ex 2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+
+/* //esercizio parte 1
+// - creare una lista di miail
+// - chiedere all utente di aggiungere una mail
+// - convalidare tramite ciclo for se la mail è presente nella lista
+// - se mail presente dichiarare che la mail è presente
 let comparison = false;
 let list_mail = ['pippobaudo@gmail.com','alberto@gmail.com','thomas@gmail.com','antoniio@gmail.com']
 let email = prompt('inserisci la tua em0ail');
@@ -12,4 +18,20 @@ if (comparison == true){
 }
 else{
     window.location.reload()
+}
+ */
+
+
+//esercizio parte 2
+
+let playerDice = Math.floor(Math.random()* 6 +1);
+let pcDice = Math.floor(Math.random()* 6 +1);
+if(playerDice > pcDice){
+    console.log("hai vinto")
+}
+else if(playerDice < pcDice){
+    console.log("hai perso")
+}
+else{
+    console.log("pareggio")
 }
